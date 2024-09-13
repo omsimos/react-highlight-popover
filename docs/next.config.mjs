e70f8a6b3af346835242d7e36b1b136569a6f7eb
Josh Daniel <joshxfi.dev@gmail.com>
2024-09-13 17:57:34 +0800

    feat: add docs page

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,4 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
+import rehypePrettyCode from "rehype-pretty-code";
 
-export default nextConfig;
+/** @type {import('rehype-pretty-code').Options} */
+const options = {
+  theme: "ayu-dark",
+  bypassInlineCode: false,
+};
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [[rehypePrettyCode, options], rehypeSlug],
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  reactStrictMode: true,
+};
+
+export default withMDX(nextConfig);
